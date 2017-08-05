09a1a1216599bae5875f1829795ba47801638642
Alexey Smagin <asblackpn@gmail.com>
2017-08-05 21:34:02 +0300

    something done

---
+++ info.js
@@ -0,0 +1,42 @@
+﻿var uniData = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    37.20848,
+                    55.98344
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+				"name": "МИЭТ",
+                "friends": 250
+            },
+            "id": 51
+        },
+		{
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    37.632843,
+                    55.761416
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+				"name": "ВШЭ",
+                "friends": 400,
+				"style": {
+					weight: 2,
+					color: "#999",
+					opacity: 1,
+					fillColor: "#B0DE5C",
+					fillOpacity: 0.8
+				}
+            },
+            "id": 51
+        }
+    ]
+};
